bdf3987503f216d558e50f7de50952f3b39e5ab4
Desiree <desireerdias@gmail.com>
2024-11-25 18:35:07 -0300

    segundo commit

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-const form = document.getElementById('form')
+const form = document.getElementById('form');
 let formSubmitted = false;
 
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
-    const res = document.getElementById('resultado')
+    
+    const res = document.getElementById('resultado');
     const campoA = parseFloat(document.getElementById('campo-a').value);
-    const campoB = parseInt(document.getElementById('campo-b').value)
-
-     if (formSubmitted) {
-        return; // Evita múltiplas submissões
-    }
+    const campoB = parseFloat(document.getElementById('campo-b').value);
 
     if(campoB > campoA){
         
         res.textContent= 'Validação feita com sucesso. Campo A é menor que Campo B'
-        campoA.value=''
-        campoB.value=''
+        campoA.value ='';
+        campoB.value ='';
+        
     } else {
         res.textContent = "ERRO: Campo A precisa ter valor menor que Campo B"
     }
-    formSubmitted = true; // Marca o formulário como enviado
+
 })
